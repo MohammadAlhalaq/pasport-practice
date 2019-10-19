6a1f864316c37aa99ccf8f038d9381af7daa1c94
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-10-19 10:32:01 +0300

    create validation input in server and set connection

---
+++ README.md
@@ -0,0 +1,9 @@
+# pasport-practice
+## install packages with ```npm i```
+## run ```npm run dev```
+
+# the tech using in this app
+ - express
+ - ejs sever side rendering
+ - passport
+
--- routes/users/users.js
+++ routes/users/users.js
@@ -1,7 +1,44 @@
-
+const { loginSc, signupSc } = require('../../schemas/userSchema');
+const { badData } = require('@hapi/boom');
 exports.login = (req, res, next) => {
-  res.render('login');
+  res.render('login', {
+    error: '',
+    path: ''
+  });
 }
 exports.register = (req, res, next) => {
-  res.render('register');
+  res.render('register', {
+    error: '',
+    path: '',
+  });
 }
+exports.dashboard = (req, res, next) => {
+  res.render('dashboard', {
+    user: {name: 'Mohammad'}
+  });
+}
+exports.postLogin = async (req, res, next) => {
+  try{
+    const result = await loginSc.validateAsync(req.body);
+    
+  }catch(err){    
+    res.render('login', {
+      error: err.details[0].message || '',
+      path: err.details[0].path[0] || ''
+    });
+}
+};
+exports.postRegister = async (req, res, next) => {
+  try{
+    const result = await signupSc.validateAsync(req.body);
+    
+  }catch(err){    
+    res.render('register', {
+      error: err.details[0].message || '',
+      path: err.details[0].path[0] || ''
+    });
+    // next(badData(`register ${err.details[0].message}`))
+  }
+  
+  
+};
