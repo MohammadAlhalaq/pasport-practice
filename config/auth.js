8c142bd7c6c0a0c5ea7d27e89f43445a249e57e6
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-10-20 00:18:12 +0300

    do one edit fake

---
+++ config/auth.js
@@ -0,0 +1,14 @@
+//for protected pages
+exports.IsLogedIn = (req, res, next) => {
+  if(req.isAuthenticated())return next();
+  req.flash('error', 'Please login to access');
+  res.redirect('/login');
+};
+
+//if user loged in he cant go to login and sign up
+exports.IsLogedOut = (req, res, next) => {
+  if(!req.isAuthenticated())return next();
+  req.flash('error', 'you cant login or sign up while you are loged in if you want replace Accounts you can logout');
+  res.redirect('/dashboard');
+};
+
