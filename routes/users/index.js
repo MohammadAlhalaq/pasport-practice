3f6bdc4a602e3f63b221385769bce1e8cf86fca7
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-10-20 00:15:33 +0300

    create passport Authentication and protected page with passport and check login

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
-const { login, register, dashboard, postLogin, postRegister } = require('./users');
+const { logout, login, register, dashboard, postLogin, postRegister } = require('./users');
 
+const { IsLogedIn, IsLogedOut} = require('../../config/auth');
 const router = express.Router();
 
-router.route('/login').get(login).post(postLogin);
-router.route('/register').get(register).post(postRegister);
+router.route('/login').get(IsLogedOut, login).post(IsLogedOut, postLogin);
+router.route('/register').get(IsLogedOut, register).post(IsLogedOut, postRegister);
+
+// for protected page
+router.use(IsLogedIn)
+router.get('/logout', logout);
 router.get('/dashboard', dashboard);
 
 module.exports = router;
